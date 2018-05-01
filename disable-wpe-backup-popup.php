d859531f2d9022ed4a57483063c13e0c98998034
Ryan Hoover <ryanshoover@gmail.com>
2018-05-01 09:11:25 -0500

    Let top-level admins see the prompt

--- disable-wpe-backup-popup.php
+++ disable-wpe-backup-popup.php
@@ -6,11 +6,29 @@
  * Author:      ryanshoover
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package disable-wpe-backup-popup
  */
 
 namespace WPEngineMods;
 
+/**
+ * Disable the WP Engine "Did you back up first?" popup
+ * by defining the appropriate constant.
+ *
+ * @return void
+ */
 function disable_wpe_backup_popup() {
+	// Let admins see the popup on single sites.
+	if ( ! is_multisite() && current_user_can( 'manage_options' ) ) {
+		return;
+	}
+
+	// Let super admins see the popup on multisites.
+	if ( is_multisite() && is_super_admin() ) {
+		return;
+	}
+
 	define( 'WPE_POPUP_DISABLED', true );
 }
 
